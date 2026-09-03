3b27e0f1250fa5a6cf16404fa4e0cd72ed6cda14
yspark <parkite576@gmail.com>
2026-09-03 20:27:28 +0900

    init

--- run_a6d6.sh
+++ run_a6d6.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 for ds in organamnist organcmnist organsmnist; do
   echo "=== Training a6d6 on $ds ==="
-  python src/train.py dataset=$ds model=a6d6
+  python src/train.py dataset=$ds model=a6d6 dataset.batch_size=64
 done
